db3593b4a3a9e2e06e4e34d69e04912a8ca5db37
aalbani <princemajood@gmail.com>
2018-05-17 13:12:15 +0300

    added customer module to store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import productsModule from './modules/productsModule'
+import customerModule from './modules/customerModule'
+
 Vue.use(Vuex)
 export const store = new Vuex.Store({
   namespaced: true,
   state: {},
   modules: {
-    products: productsModule
+    products: productsModule,
+    customer: customerModule
   },
   mutations: {},
   actions: {},
