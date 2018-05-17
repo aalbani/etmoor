185331181f0388c239b3aad4049a9e4ab3f815b3
aalbani <princemajood@gmail.com>
2018-05-17 13:06:35 +0300

    added getCustomer to firestore

--- src/store/firebase/firestore.js
+++ src/store/firebase/firestore.js
@@ -17,5 +17,24 @@ export default {
       })
       cb(array)
     }).catch(error => console.log(error))
+  },
+  getCustomer (array, cb) {
+    firestore.collection('CUSTOMER').get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(document => {
+        const data = {
+          'id': document.id,
+          'city': document.data().city,
+          'country': document.data().country,
+          'county': document.data().county,
+          'hood': document.data().hood,
+          'phone': document.data().phone,
+          'khalas_quant': document.data().khalas_quant,
+          'sokry_quant': document.data().sokry_quant
+        }
+        array.push(data)
+      })
+      cb(array)
+    }).catch(error => console.log(error))
   }
 }
