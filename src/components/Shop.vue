ccff7716b48b59b2413c04227abd1d08301ba2d0
aalbani <princemajood@gmail.com>
2018-05-20 14:32:57 +0300

    playing with v-model shop

--- src/components/Shop.vue
+++ src/components/Shop.vue
@@ -4,7 +4,7 @@ under the box will have add to cart + count -->
 <v-container grid-list-lg id="container">
 
   <v-layout row wrap>
-    <v-flex xs12 sm6 v-for="product in products" :key="product.id">
+    <v-flex xs12 sm6 v-for="product in products" :key="product.id" >
       <v-card>
         <v-card-media
         height="200px"
@@ -18,8 +18,19 @@ under the box will have add to cart + count -->
          <h3> {{product.arabicTitle}} </h3>
         </div>
         </v-card-title>
-        <v-card-actions>
-          <v-btn block outline color="primary">اضف الى السلة</v-btn>
+        <v-card-actions class="justify-center">
+              <v-dialog  v-show="true" persistent max-width="290">
+                <v-btn slot="activator" color="primary" large outline dark>اضافة الى السلة</v-btn>
+                <v-card >
+                  <v-card-title class="headline">{{product.dateType}}</v-card-title>
+                  <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="green darken-1" flat @click.native="dialog = false">Disagree</v-btn>
+                    <v-btn color="green darken-1" flat @click.native="dialog = false">Agree</v-btn>
+              </v-card-actions>
+                </v-card>
+              </v-dialog>
         </v-card-actions>
       </v-card>
     </v-flex>
@@ -32,6 +43,7 @@ under the box will have add to cart + count -->
   export default {
     data () {
       return {
+        dialog: false,
     }
     },
     computed: {
