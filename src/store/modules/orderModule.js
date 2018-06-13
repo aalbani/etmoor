4fd6ab65bc5e98c2d863752ccf22009b54e85928
aalbani <princemajood@gmail.com>
2018-06-13 17:05:02 +0300

    rebased to oldr version

--- src/store/modules/orderModule.js
+++ src/store/modules/orderModule.js
@@ -1,3 +1,4 @@
+
 import firebase from '../firebase/firestore'
 
 // LEVEL 0 : CUSTOMER, LEVEL 1 :  PROVIDOR, LEVEL 2 : ADMIN.
@@ -15,10 +16,10 @@ const mutations = {
   }
 }
 const actions = {
-  addNewOrder ({commit}, orderForm) {
+  addNewOrder ({ commit }, orderForm) {
     return firebase.addNewOrder(orderForm)
   },
-  initDashboard ({commit}) {
+  initDashboard ({ commit }) {
     firebase.getOrders([], cb => {
       commit('setOrders', cb.array)
       commit('setCount', cb.count)
